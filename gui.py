6c778b2b71c32ac9768b25b518d618120285cd1e
Joonas Lahtinen <joonas.l.lahtinen@tuni.fi>
2022-01-24 01:44:21 +0200

    Version 0.5 beta

--- gui.py
+++ gui.py
@@ -6,73 +6,75 @@ from inspectview import *
 sg.theme('DarkAmber')
 
 tracking = False
-searchOn = sg.Text('Idle', background_color='Red', pad=(20,10))
+searchOn = sg.Text('Idle', background_color='Red', pad=(20,10), size=(6, 1))
 threadList = sg.Listbox([], size=(60,10), enable_events=True, key='-list-')
+urlBox = sg.InputText('', key='siteUrl')
+trackbtn = sg.Button('Track', disabled=False)
+untrackbtn = sg.Button('Untrack', disabled=True)
+caseCheckbox = sg.Checkbox('Case sensitive', key='-case-sensitive-')
 titleList = []
 altList = []
 url = ''
+keywrds = sg.InputText('', key='input_words')
 
 layout = [
     [sg.Text('Input the .rss URL of the feed you want to track')],
-    [sg.InputText('', key='siteUrl')],
+    [urlBox],
     [sg.Text('Input keywords for tracking (separate words with spaces)')],
-    [sg.InputText('', key='input_words')],
+    [keywrds, caseCheckbox],
     [sg.Text('Threads that match the criteria')],
     [threadList],
-    [sg.Button('Track'), sg.Button('Untrack'), sg.Button('Close'), sg.Button('Info')],
-    [searchOn, sg.Checkbox('Alerts', enable_events=True, pad=(40,10), key='-alerts-')]
+    [trackbtn, untrackbtn, sg.Button('Info') , sg.Button('Exit')],
+    [searchOn, sg.Checkbox('Alerts', enable_events=True, pad=(40,10), key='-alerts-')],
+    [sg.Text('Made by Joonas Lahtinen - Source code available at GitHub', font=('Any 9'), text_color='gray')]
 ]
 
-window = sg.Window('RSS Alerter', layout, size=(500, 400), icon='Img/icon.png')
-window.finalize()
+window = sg.Window('RSS Alerter', layout, size=(500, 425), icon='icon.ico', finalize=True)
 
+#converting user input into list of filter strings
 def inputParser (input):
     resultingList = list(input.split(' '))
     return resultingList
 
-def updateList (originalList):
-    altList = getTitles(inputParser(values['input_words']))
-    
-    if areThereNewTitles(altList, originalList):
-        return True
-    
-    time.sleep(5)
-    print('updatelist')
-    updateList()
 
-def areThereNewTitles (list1, list2):
-    if list1 != list2:
-        if values['-alerts-'] == True:
-            print('\a')
-        return True
-    return False
-
-
-
+#Main program loop. When broken -> program is terminated
 while True:
-    event, values = window.Read(timeout=100)
+    event, values = window.read()
     
-    if event == 'Close' or event == sg.WIN_CLOSED:
+    if event == 'Exit' or event == sg.WIN_CLOSED:
         break
         
     if event == 'Track':
         tracking = True
         url = values['siteUrl']
         searchOn.update('Tracking', background_color='Green')
-        titleList = getTitles(inputParser(values['input_words']), url)
+        titleList = getTitles(inputParser(values['input_words']), url, values['-case-sensitive-'])
         threadList.update(titleList)
+        trackbtn.update(disabled=True)
+        untrackbtn.update(disabled=False)
+        caseCheckbox.update(disabled=True)
+        urlBox.update(disabled=True)
+        keywrds.update(disabled=True)        
               
     if event == 'Untrack':
         tracking = False
         searchOn.update('Idle', background_color='Red')
         threadList.update([])
+        trackbtn.update(disabled=False)
+        untrackbtn.update(disabled=True)
+        caseCheckbox.update(disabled=False)
+        urlBox.update(disabled=False)
+        keywrds.update(disabled=False) 
         
     if event == 'Info':
-        selected = window['-list-'].get()
-        popup(selected, values['siteUrl'])
+        try:
+            selected = window['-list-'].get()
+            popup(selected, values['siteUrl'])
+        except:
+            print('No item selected')
         
           
-    altList = getTitles(inputParser(values['input_words']), url)
+    altList = getTitles(inputParser(values['input_words']), url, values['-case-sensitive-'])
     
     if altList != titleList and tracking == True:
     
@@ -81,17 +83,17 @@ while True:
             
         titleList = altList
         threadList.update(titleList)
-        threadList = window['ListBox']
-        index = threadList.GetIndexes()[0]
-        threadList.Widget.itemconfig(index, fg='red', bg='green')
         
+        try:
+            threadList.Widget.itemconfig(0, fg='yellow', bg='green')
+        except:
+            print('Index not found')
+    
     window.refresh()
-        
+    
 window.close()
 
 
-
-
-#is feed url -- https://www.is.fi/rss/tuoreimmat.xml
-
-#techbbs prossut linkki -- https://bbs.io-tech.fi/forums/prosessorit-emolevyt-ja-muistit.73/index.rss
+#FINNISH RSS FEEDS FOR TESTING PURPOSES LINKED BELOW
+#news feed url -- https://www.is.fi/rss/tuoreimmat.xml
+#techbbs -- https://bbs.io-tech.fi/forums/prosessorit-emolevyt-ja-muistit.73/index.rss
--- inspectview.py
+++ inspectview.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 import html2text
 from feedhandler import *
 
+#function converts raw html from the feed summary to plaintext
 def convertHtml(string):
     tt = html2text.HTML2Text()
     tt.ignore_links = True
@@ -10,25 +11,29 @@ def convertHtml(string):
     retrn = tt.handle(string)
     return retrn
 
+#fetching info about the feed items
 def getInfo(itemTitle, feedUrl):
     infoList = getAllInfo(itemTitle, feedUrl)
     return infoList
-    
+
+#opens default web browser and redirects to given url    
 def openLink(url):
     webbrowser.open(url)
 
+#opens popup window that contains info about feed item of users choice.
+#window closes when after this function
 def popup(item, url):
     sg.theme('DarkAmber')
     
     infoList = getInfo(item, url)
     
     layout = [
-        [sg.Text('Link: ', font=('bold'), text_color='White'), sg.Text(infoList[0], pad=(5,5)), sg.Button('Visit')],
+        [sg.Text('Link: ', font=('bold'), text_color='White'), sg.Text(infoList[0], pad=(5,5), size=(200,1))],
         [sg.Text('Item title: ', font=('bold'), text_color='White'), sg.Text(infoList[1])],
         [sg.Text('Published: ', font=('bold'), text_color='White'), sg.Text(infoList[2])],
         [sg.Text('Summary: ', font=('bold'), text_color='White')],
-        [sg.Multiline(convertHtml(infoList[3]), size=(70, 10))], 
-        [sg.Button('Close')]
+        [sg.Multiline(convertHtml(infoList[3]), size=(70, 10), disabled=True)], 
+        [sg.Button('Close'), sg.Button('Visit site')]
     ]
 
     window = sg.Window('Inspect', layout, size=(500, 350))
@@ -39,7 +44,7 @@ def popup(item, url):
         if event == 'Close' or event == sg.WIN_CLOSED:
             break
 
-        if event == 'Visit':
+        if event == 'Visit site':
             openLink(infoList[0])
             
     window.close()
